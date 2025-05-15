cc3c7d13892dc3a8b13bbdb552ee03ba33eb3c45
Your Name <nakuleshj1998@gmail.com>
2025-05-15 17:09:28 -0400

    Remove warehouse IDs from data

--- scripts/forecast_demand.py
+++ scripts/forecast_demand.py
@@ -3,22 +3,35 @@ import pandas as pd
 import sqlite3
 from prophet import Prophet
 import matplotlib.pyplot as plt
+import logging
+from tqdm import tqdm
+
+logger = logging.getLogger('cmdstanpy')
+logger.setLevel(logging.ERROR)
+logger.addHandler(logging.NullHandler())
+
+def forecaster(df):
+    prophet_model=Prophet()
+    prophet_model.fit(df)
+    predictions_ds=prophet_model.make_future_dataframe(periods=60)
+    forecast = prophet_model.predict(predictions_ds)
+    forecast=forecast[forecast['ds'].isin(df['ds'].values)==False]
+    print(forecast.shape[0])
+    forecast['yhat']=forecast['yhat'].astype(int)
+    forecast_dict={'y':forecast['yhat'].values,'ds':forecast['ds'].values}
+    return forecast_dict
 
 conn=sqlite3.connect('../database/inventory.db')
-daily_volume=pd.read_sql('SELECT timestamp as ds,product_id,SUM(units_sold) as y FROM sales GROUP BY timestamp, product_id',conn)
+sales_data=pd.read_sql('SELECT timestamp as ds,product_id,SUM(units_sold) as y FROM sales GROUP BY timestamp, product_id',conn)
 conn.close()
 
 #converting to time-series data
 
-daily_volume['ds']=pd.to_datetime(daily_volume['ds'])
-#daily_volume.set_index('ds',inplace=True)
+sales_data['ds']=pd.to_datetime(sales_data['ds'])
+sales_data.dropna(inplace=True)
+product_ids=sales_data['product_id'].unique()
 
-prophet_model=Prophet()
-prophet_model.fit(daily_volume)
-
-predictions=prophet_model.make_future_dataframe(periods=30)
-
-forecast = prophet_model.predict(predictions)
-
-prophet_model.plot(forecast)
-plt.show()
+for pid in product_ids:
+    if len(sales_data[sales_data['product_id']==pid]) >=2:
+        forecasted_values=pd.DataFrame(forecaster(sales_data[sales_data['product_id']==pid]))
+        forecasted_values['product_id']=pid
