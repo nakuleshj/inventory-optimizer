25d3278f5e42f4490e9bf8ad885b2e7876cddad0
Your Name <nakuleshj1998@gmail.com>
2025-05-15 14:00:28 -0400

    Write script to forecast unit volume demand using Prophet

--- scripts/forecast_demand.py
+++ scripts/forecast_demand.py
@@ -1,15 +1,24 @@
 import numpy as np
 import pandas as pd
 import sqlite3
+from prophet import Prophet
+import matplotlib.pyplot as plt
 
 conn=sqlite3.connect('../database/inventory.db')
-daily_volume=pd.read_sql('SELECT timestamp,product_id,SUM(units_sold) as daily_volume FROM sales GROUP BY timestamp, product_id',conn)
+daily_volume=pd.read_sql('SELECT timestamp as ds,product_id,SUM(units_sold) as y FROM sales GROUP BY timestamp, product_id',conn)
 conn.close()
 
 #converting to time-series data
 
-daily_volume['timestamp']=pd.to_datetime(daily_volume['timestamp'])
-daily_volume.set_index('timestamp',inplace=True)
+daily_volume['ds']=pd.to_datetime(daily_volume['ds'])
+#daily_volume.set_index('ds',inplace=True)
 
-if daily_volume.isnull().sum() > 0:
-    daily_volume.dropna()
+prophet_model=Prophet()
+prophet_model.fit(daily_volume)
+
+predictions=prophet_model.make_future_dataframe(periods=30)
+
+forecast = prophet_model.predict(predictions)
+
+prophet_model.plot(forecast)
+plt.show()
