f6660c93acb9e996029984f4f6ad35e9e83b8536
Your Name <nakuleshj1998@gmail.com>
2025-05-16 17:01:57 -0400

    Retrieve forecasted demand from database

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -3,3 +3,13 @@ import sqlite3
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
+conn=sqlite3.connect('../data/inevntory.db')
+forecasted_demand=pd.read_sql('SELECT * FROM forecasts',conn)
+conn.close()
+
+forecasted_demand_pivot=pd.pivot_table(
+    forecasted_demand,
+    index=['product_id','ds'],
+)
+
+print(forecasted_demand.head())
